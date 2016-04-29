bb43e34e7580e4aad49e457c94e78c76366791e7
Blake Polidore <blakepolidore@gmail.com>
2016-04-29 15:19:26 -0700

    Added retrofit library

---
+++ Project4/app/src/main/java/blake/com/project4/Keys.java
@@ -0,0 +1,12 @@
+package blake.com.project4;
+
+/**
+ * Created by Raiders on 4/29/16.
+ */
+public class Keys {
+
+    public static final String YELP_CONSUMER_KEY = "OnPTZobs9hu3nWN6o2ZYiQ";
+    public static final String YELP_CONSUMER_SECRET = "RO5Q47AhFcGb48G9B7f8QPY5mMo";
+    public static final String YELP_TOKEN = "UXvnSOVdA6QL6tJviycbgoAmTgWxYxWt";
+    public static final String YELP_TOKEN_SECRET = "MR7EzZOn-2G6pcIm8kSSSbpcmlk";
+}
---
+++ Project4/app/src/main/java/blake/com/project4/apicalls/YelpAPIService.java
@@ -0,0 +1,9 @@
+package blake.com.project4.apicalls;
+
+/**
+ * Created by Raiders on 4/29/16.
+ */
+public interface YelpAPIService {
+
+
+}
