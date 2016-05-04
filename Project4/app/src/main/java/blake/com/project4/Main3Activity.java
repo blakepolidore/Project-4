b4ca805af9843e9b635385f89b5d324e22f78258
Blake Polidore <blakepolidore@gmail.com>
2016-05-03 22:12:28 -0700

    added booleans to the shared preferences

--- Project4/app/src/main/java/blake/com/project4/Main3Activity.java
+++ Project4/app/src/main/java/blake/com/project4/Main3Activity.java
@@ -1,6 +1,8 @@
 package blake.com.project4;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,9 +13,38 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ScrollView;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 public class Main3Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navigationView;
+    private ScrollView scrollView;
+    private EditText locationEditText;
+    private SeekBar radiusSeekbar;
+    private Switch deviceLocationSwitch;
+    private Switch foodSwitch;
+    private Switch drinkSwitch;
+    private Switch locationsSwitch;
+    private Switch eventsSwitch;
+
+    private SharedPreferences sharedPreferences;
+
+    private boolean deviceLocationToggle = false;
+    private boolean foodQueryToggle = false;
+    private boolean drinkQueryToggle = false;
+    private boolean locationQueryToggle = false;
+    private boolean eventsQueryToggle = false;
+
+    private final String FOOD_BOOLEAN_CODE = "food";
+    private final String DRINK_BOOLEAN_CODE = "drink";
+    private final String LOCATION_BOOLEAN_CODE = "location";
+    private final String EVENTS_BOOLEAN_CODE = "events";
+    private final String DEVICE_LOCATION_BOOLEAN_CODE = "device";
+
+    private String locationInput;
+    private final String LOCATION_INPUT_CODE = "user input";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +58,28 @@ public class Main3Activity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        //navigationView.setNavigationItemSelectedListener(this);
-
-        ScrollView scrollView = (ScrollView) navigationView.findViewById(R.id.nav_scrollview);
-
-        EditText editText = (EditText) scrollView.findViewById(R.id.location_editText);
+        setViews();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        locationSelection();
+        checkVenueSwitches(foodSwitch);
+        checkVenueSwitches(drinkSwitch);
+        checkVenueSwitches(locationsSwitch);
+        checkVenueSwitches(eventsSwitch);
 
 //        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 //        drawer.closeDrawer(GravityCompat.START);
+    }
+
+    private void setViews() {
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        scrollView = (ScrollView) navigationView.findViewById(R.id.nav_scrollview);
+        locationEditText = (EditText) scrollView.findViewById(R.id.location_editText);
+        radiusSeekbar = (SeekBar) scrollView.findViewById(R.id.search_radius_seekbar);
+        deviceLocationSwitch = (Switch) scrollView.findViewById(R.id.phone_location_switch);
+        foodSwitch = (Switch) scrollView.findViewById(R.id.food_search_switch);
+        drinkSwitch = (Switch) scrollView.findViewById(R.id.drink_search_switch);
+        locationsSwitch = (Switch) scrollView.findViewById(R.id.activities_search_switch);
+        eventsSwitch = (Switch) scrollView.findViewById(R.id.events_search_switch);
     }
 
     @Override
@@ -64,15 +107,84 @@ public class Main3Activity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void locationSelection() {
+        if (deviceLocationSwitch.isChecked()) {
+            deviceLocationToggle = true;
+            locationEditText.setEnabled(false);
+        } else {
+            deviceLocationToggle = false;
+            locationEditText.setEnabled(true);
+        }
+    }
+
+    private void checkVenueSwitches(Switch s) {
+        boolean toggled = s.isChecked();
+        switch (s.getId()) {
+            case R.id.food_search_switch:
+                if (toggled) {
+                    foodQueryToggle = true;
+                } else {
+                    foodQueryToggle = false;
+                }
+                break;
+            case R.id.drink_search_switch:
+                if (toggled) {
+                    drinkQueryToggle = true;
+                } else {
+                    drinkQueryToggle = false;
+                }
+                break;
+            case R.id.activities_search_switch:
+                if (toggled) {
+                    locationQueryToggle = true;
+                } else {
+                    locationQueryToggle = false;
+                }
+                break;
+            case R.id.events_search_switch:
+                if (toggled) {
+                    eventsQueryToggle = true;
+                } else {
+                    eventsQueryToggle = false;
+                }
+                break;
+            default:
+        }
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         return false;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(FOOD_BOOLEAN_CODE, foodQueryToggle);
+        editor.putBoolean(DRINK_BOOLEAN_CODE, drinkQueryToggle);
+        editor.putBoolean(LOCATION_BOOLEAN_CODE, locationQueryToggle);
+        editor.putBoolean(EVENTS_BOOLEAN_CODE, eventsQueryToggle);
+        editor.putBoolean(DEVICE_LOCATION_BOOLEAN_CODE, deviceLocationToggle);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        foodQueryToggle = sharedPreferences.getBoolean(FOOD_BOOLEAN_CODE, foodQueryToggle);
+        foodSwitch.setChecked(foodQueryToggle);
+        drinkQueryToggle = sharedPreferences.getBoolean(DRINK_BOOLEAN_CODE, drinkQueryToggle);
+        drinkSwitch.setChecked(drinkQueryToggle);
+        locationQueryToggle = sharedPreferences.getBoolean(LOCATION_BOOLEAN_CODE, locationQueryToggle);
+        locationsSwitch.setChecked(locationQueryToggle);
+        eventsQueryToggle = sharedPreferences.getBoolean(EVENTS_BOOLEAN_CODE, eventsQueryToggle);
+        eventsSwitch.setChecked(eventsQueryToggle);
+        deviceLocationToggle = sharedPreferences.getBoolean(DEVICE_LOCATION_BOOLEAN_CODE, deviceLocationToggle);
+        deviceLocationSwitch.setChecked(deviceLocationToggle);
     }
 }
