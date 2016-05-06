474a7d27ba2581cac3024d9fe0eaa1e18db36e01
Blake Polidore <blakepolidore@gmail.com>
2016-05-06 16:39:38 -0700

    added touch listener need to fix buttons in it and be able to delete items from firebase

--- Project4/app/src/main/java/blake/com/project4/LikedActivity.java
+++ Project4/app/src/main/java/blake/com/project4/LikedActivity.java
@@ -1,5 +1,6 @@
 package blake.com.project4;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
@@ -16,10 +18,13 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageSize;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import blake.com.project4.cardModelAndAdapter.Cards;
+import blake.com.project4.feedRecyclerviewAdapter.ClickListener;
 import blake.com.project4.feedRecyclerviewAdapter.LikedFeedAdapter;
+import blake.com.project4.feedRecyclerviewAdapter.RecyclerTouchListener;
 
 /**
  * Activity that shows all the users favorited cards
@@ -28,7 +33,7 @@ public class LikedActivity extends AppCompatActivity {
 
     private static final String TAG = "LikedActivity: ";
 
-    private List<Cards> cardsList;
+    private List<Cards> cardsList = new ArrayList<>();
     private LikedFeedAdapter likedFeedAdapter;
     private RecyclerView recyclerView;
     private ImageLoader imageLoader = ImageLoader.getInstance();
@@ -44,6 +49,7 @@ public class LikedActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         setLikedCards();
+        setListClickListener();
     }
 
     /**
@@ -73,6 +79,7 @@ public class LikedActivity extends AppCompatActivity {
                 imageLoader.init(ImageLoaderConfiguration.createDefault(getApplicationContext()));
                 ImageSize imageSize = new ImageSize(100, 100);
                 imageLoader.displayImage(imageUrl, feedViewHolder.image, imageSize);
+                cardsList.add(cards);
             }
         };
 
@@ -92,6 +99,41 @@ public class LikedActivity extends AppCompatActivity {
         AuthData authData = firebase.getAuth();
         String uID = authData.getUid();
         return uID;
+    }
+
+    private void setListClickListener() {
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Intent venueIntent = new Intent(LikedActivity.this, VenueActivity.class);
+                venueIntent.putExtra(Main3Activity.TITLE_TEXT, cardsList.get(position).getTitle());
+                venueIntent.putExtra(Main3Activity.CATEGORY_TEXT, cardsList.get(position).getCategory());
+                venueIntent.putExtra(Main3Activity.IMAGE_TEXT, cardsList.get(position).getImageUrl());
+                venueIntent.putExtra(Main3Activity.LOCATION_TEXT, cardsList.get(position).getLocation());
+                venueIntent.putExtra(Main3Activity.DESCRIPTION_TEXT, cardsList.get(position).getDescription());
+                venueIntent.putExtra(Main3Activity.PHONE_TEXT, cardsList.get(position).getPhone());
+                venueIntent.putExtra(Main3Activity.WEBSITE_TEXT, cardsList.get(position).getWebsite());
+                startActivityForResult(venueIntent, Main3Activity.INTENT_FOR_RESULT);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data != null) {
+            Boolean venueBoolean = data.getBooleanExtra(VenueActivity.IF_LIKE_INTENT, false);
+            if (venueBoolean) {
+
+            } else {
+
+            }
+        }
     }
 }
 
---
+++ Project4/app/src/main/java/blake/com/project4/feedRecyclerviewAdapter/ClickListener.java
@@ -0,0 +1,11 @@
+package blake.com.project4.feedRecyclerviewAdapter;
+
+import android.view.View;
+
+/**
+ * Created by Raiders on 5/6/16.
+ */
+public interface ClickListener {
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
