c29ad732df2ac6d643b907f80a17e1bfdae66498
Blake Polidore <blakepolidore@gmail.com>
2016-05-12 12:56:58 -0700

    before push

--- Project4/app/src/main/java/blake/com/project4/apicalls/RottenTomatoesAPIService.java
+++ Project4/app/src/main/java/blake/com/project4/apicalls/RottenTomatoesAPIService.java
@@ -1,6 +1,6 @@
 package blake.com.project4.apicalls;
 
-import blake.com.project4.foursquareModel.Root;
+import blake.com.project4.models.rottenTomatoesModel.rottenTomatoesModel.RTRoot;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -11,5 +11,5 @@ import retrofit2.http.Query;
 public interface RottenTomatoesAPIService {
 
     @GET("in_theaters.json")
-    Call<Root> searchInTheaters(@Query("apikey") String apiKey, @Query("page_limit") String limit);
+    Call<RTRoot> searchInTheaters(@Query("apikey") String apiKey, @Query("page_limit") String limit);
 }
